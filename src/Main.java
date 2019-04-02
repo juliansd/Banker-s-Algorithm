b5cc9ba6ea0eb3a2f8941265607083fe3cdd7d78
Julian Smith De Niro <jsd410@nyu.edu>
2019-04-02 16:11:48 -0400

    correctly processing input

--- src/Main.java
+++ src/Main.java
@@ -3,99 +3,63 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	
-	private ArrayList<Object> processedInput;
+
+	private ArrayList<Resource> resources;
+	private int[][] processedInput;
 	private int numOfTasks;
-	private int numOfResources;
-	private int resourceVal;
 	
 	@SuppressWarnings("resource")
 	protected void processInput(String filePath) {
 		
-		ArrayList<Object> processedInput = new ArrayList<Object>();
-		
 		Scanner scan = new Scanner(System.in);
+		
+		ArrayList<Resource> resources = new ArrayList<Resource>();
 		
 		File file = new File(filePath);
 		
+		int count = 0;
+		int row = 0;
+		int col = 0;
+		
+		int[][] processedInput = null;
+		
 		try {
-			ArrayList<Task> tasks = new ArrayList<Task>();
-			ArrayList<Resource> resources = new ArrayList<Resource>();
+			
 			scan = new Scanner(file);
-			Task t = new Task();
-			int[] instanceVar = new int[4]; 
-			int count = 0;
-			int count2 = 0;
-			String dataState = null;
+			
 			while (scan.hasNext()) {
 				String current = scan.next();
 				if (count == 0) {
 					this.setNumOfTasks(Integer.parseInt(current));
 					count++;
 				} else if (count == 1) {
-					this.setNumOfResources(Integer.parseInt(current));
-					count++;
-				} else if (count == 2) {
-					this.setResourceVal(Integer.parseInt(current));
-					count++;
+					int differentResourceTypes = Integer.parseInt(current);
+					for (int i = 0; i < differentResourceTypes; i++) {
+						Resource r = new Resource(Integer.parseInt(scan.next()));
+						resources.add(r);
+					}
+					this.setResources(resources);
+					count = -1;
+					processedInput = new int[(this.getResources().size()*this.getNumOfTasks()*3)+this.getNumOfTasks()][4];
 				} else {
-					if (current.equals("initiate"))
-						t = new Task();
-					
-					if (count2 % 5 == 0) {
-						instanceVar = new int[4];
-						dataState = current;
-					}
-					
-					else if (count2 % 5 == 1)
-						instanceVar[0] = Integer.parseInt(current);
-					
-					else if (count2 % 5 == 2)
-						instanceVar[1] = Integer.parseInt(current);
-					
-					else if (count2 % 5 == 3)
-						instanceVar[2] = Integer.parseInt(current);
-					
-					else {
-						instanceVar[3] = Integer.parseInt(current);
-						if (dataState.equals("initiate"))
-							t.setInitiate(instanceVar);
+					if (current.length() == 1) {
+						processedInput[row][col] = Integer.parseInt(current);
 						
-						else if (dataState.equals("request"))
-							t.setRequest(instanceVar);
-						
-						else if (dataState.equals("release"))
-							t.setRelease(instanceVar);
-						
-						else {
-							t.setTerminate(instanceVar[0]);
-							tasks.add(t);
+						if (col == 3) {
+							col = 0;
+							row++;
+						} else {
+							col++;
 						}
+						if (row == (this.getResources().size()*this.getNumOfTasks()*3)+this.getNumOfTasks()-1 && col == 3)
+							break;
 					}
-					
-					count2++;
 				}
 			}
-			for (int i = 0; i < this.getNumOfResources(); i++) {
-				Resource r = new Resource(this.getResourceVal());
-				resources.add(r);
-			}
 			
-			processedInput.add(tasks);
-			processedInput.add(resources);
-			scan.close();
 			this.setProcessedInput(processedInput);
 			
 		} catch (Exception e) {}
-		
-	}
-	
-	protected void BankersAlgorithm() {
-		@SuppressWarnings("unchecked")
-		ArrayList<Task> tasks = (ArrayList<Task>) this.getProcessedInput().get(0);
-		@SuppressWarnings("unchecked")
-		ArrayList<Resource> resources = (ArrayList<Resource>) this.getProcessedInput().get(1);
-		
 		
 	}
 	
@@ -105,36 +69,28 @@ public class Main {
 		
 	}
 	
-	public void setNumOfTasks(int numOfTasks) {
-		this.numOfTasks = numOfTasks;
+	public ArrayList<Resource> getResources() {
+		return resources;
 	}
 	
-	public int getNumOfTasks() {
-		return this.numOfTasks;
+	public void setResources(ArrayList<Resource> resources) {
+		this.resources = resources;
 	}
-
-	public int getNumOfResources() {
-		return numOfResources;
-	}
-
-	public void setNumOfResources(int numOfResources) {
-		this.numOfResources = numOfResources;
-	}
-
-	public int getResourceVal() {
-		return resourceVal;
-	}
-
-	public void setResourceVal(int resourceVal) {
-		this.resourceVal = resourceVal;
-	}
-
-	public ArrayList<Object> getProcessedInput() {
+	
+	public int[][] getProcessedInput() {
 		return processedInput;
 	}
-
-	public void setProcessedInput(ArrayList<Object> processedInput) {
+	
+	public void setProcessedInput(int[][] processedInput) {
 		this.processedInput = processedInput;
+	}
+
+	public int getNumOfTasks() {
+		return numOfTasks;
+	}
+
+	public void setNumOfTasks(int numOfTasks) {
+		this.numOfTasks = numOfTasks;
 	}
 	
 }
--- src/Task.java
+++ src/Task.java
@@ -1,50 +1,43 @@
 
 public class Task {
 
-	private int[] initiate;
-	private int[] request;
-	private int[] release;
+	private int[][] initiate;
+	private int[][] request;
+	private int[][] release;
 	private int terminate;
 	
 	public Task() {}
 
-	public Task(int[] initiate, int[] request, int[] release, int terminate) {
-		this.setInitiate(initiate);
-		this.setRequest(request);
-		this.setRelease(release);
-		this.setTerminate(terminate);
-	}
-	
-	public int[] getInitiate() {
+	public int[][] getInitiate() {
 		return initiate;
 	}
-	
-	public void setInitiate(int[] initiate) {
+
+	public void setInitiate(int[][] initiate) {
 		this.initiate = initiate;
 	}
-	
-	public int[] getRequest() {
+
+	public int[][] getRequest() {
 		return request;
 	}
-	
-	public void setRequest(int[] request) {
+
+	public void setRequest(int[][] request) {
 		this.request = request;
 	}
-	
-	public int[] getRelease() {
+
+	public int[][] getRelease() {
 		return release;
 	}
-	
-	public void setRelease(int[] release) {
+
+	public void setRelease(int[][] release) {
 		this.release = release;
 	}
-	
+
 	public int getTerminate() {
 		return terminate;
 	}
-	
+
 	public void setTerminate(int terminate) {
 		this.terminate = terminate;
 	}
-	
+
 }
