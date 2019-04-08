4dfebb7c02803c1c62d3bd1c97e0206d6891cf43
Julian Smith De Niro <jsd410@nyu.edu>
2019-04-08 14:06:19 -0400

    cleaned up the processing of the input

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-	private ArrayList<Resource> resources;
-	private int[][][] bankersTable;
+	private HashMap<Task, int[][]> bankersTable;
 	private ArrayList<Task> tasks;
+	private ArrayList<Task> tasks2;
 	private int[][] processedInput;
 	private int[] total;
 	private int[] available;
@@ -15,11 +22,13 @@ public class Main {
 	private int mod;
 	
 	@SuppressWarnings("resource")
-	protected void processInput(String filePath) {
+	protected ArrayList<ArrayList<Object>> processInput(String filePath) {
+		
+		ArrayList<ArrayList<Object>> output = new ArrayList<ArrayList<Object>>();
+		ArrayList<Object> resources = new ArrayList<Object>();
+		ArrayList<Object> tasks = new ArrayList<Object>();
 		
 		Scanner scan = new Scanner(System.in);
-		
-		ArrayList<Resource> resources = new ArrayList<Resource>();
 		
 		File file = new File(filePath);
 		
@@ -33,250 +42,60 @@ public class Main {
 			
 			scan = new Scanner(file);
 			
-			while (scan.hasNext()) {
-				String current = scan.next();
-				if (count == 0) {
-					this.setNumOfTasks(Integer.parseInt(current));
-					count++;
-				} else if (count == 1) {
-					int differentResourceTypes = Integer.parseInt(current);
-					for (int i = 0; i < differentResourceTypes; i++) {
-						Resource r = new Resource(Integer.parseInt(scan.next()));
-						resources.add(r);
+			Path path = Paths.get(filePath);
+			byte[] bytes = Files.readAllBytes(path);
+			List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+			System.out.println(allLines);
+			
+			LinkedList<ArrayList<String>> instructions = new LinkedList<ArrayList<String>>();
+			Task t = new Task();
+			
+			for (int i = 0; i < allLines.size(); i++) {
+				String[] line = allLines.get(i).split(" ");
+				if (i == 0) {
+					int numOfTasks = Integer.parseInt(line[0]);
+					this.setNumOfTasks(numOfTasks);
+					int numOfResources = Integer.parseInt(line[1]);
+					this.setNumOfResources(numOfResources);
+					for (int j = 2; j < line.length; j++) {
+						resources.add(Integer.parseInt(line[j]));
 					}
-					this.setResources(resources);
-					this.setNumOfResources(differentResourceTypes);
-					count = -1;
-					processedInput = new int[(this.getResources().size()*this.getNumOfTasks()*3)+this.getNumOfTasks()][4];
 				} else {
-					if (current.length() == 1) {
-						processedInput[row][col] = Integer.parseInt(current);
-						
-						if (col == 3) {
-							col = 0;
-							row++;
-						} else {
-							col++;
-						}
-						if (row == (this.getResources().size()*this.getNumOfTasks()*3)+this.getNumOfTasks()-1 && col == 3)
-							break;
+					ArrayList<String> newLine = new ArrayList<String>();
+					for (int j = 0; j < line.length; j++) {
+						if (!line[j].equals(""))
+							newLine.add(line[j]);
+					}
+					instructions.addLast(newLine);
+					if (line[0].equals("terminate")) {
+						t.setInstructions(instructions);
+						tasks.add(t);
+						t = new Task();
+						instructions = new LinkedList<ArrayList<String>>();
 					}
 				}
 			}
-			
-			this.setMod((this.getNumOfResources()*3)+1);
-			this.setProcessedInput(processedInput);
+			output.add(tasks);
+			output.add(resources);
 			
 		} catch (Exception e) {}
-		
+		scan.close();
+		return output;
 	}
 	
-	protected void populateBankersTable() {
-		int numOfResources = this.getNumOfResources();
-		int[][][] bankersTable = new int[this.getNumOfTasks()][3][numOfResources];
-		int[][] processedInput = this.getProcessedInput();
-		int[][] initialClaim = new int[this.getNumOfTasks()][numOfResources];
-		int[] total = new int[numOfResources];
-		int[] available = new int[numOfResources];
-		int[] currentClaim = new int[numOfResources];
-		int m = numOfResources*3 + 1;
-		int n = 0;
-		int p = 0;
-		
-		for (int i = 0; i < processedInput.length; i++) {
-			if (i % m >= 0 && i % m < numOfResources) {
-				
-				currentClaim[n] = processedInput[i][3];
-				n++;
-				
-				if (n == currentClaim.length)
-					n = 0;
-			}
-			
-			else if (i % m == numOfResources) {
-				initialClaim[p] = currentClaim;
-				p++;
-				currentClaim = new int[numOfResources];
-			}
-		}
-		
-		for (int i = 0; i < bankersTable.length; i++) {
-			for (int j = 0; j < bankersTable[i].length; j++) {
-				if (j == 0 || j == 2)
-					bankersTable[i][j] = initialClaim[i];
-			}
-		}
-		
-		for (int i = 0; i < available.length; i++) {
-			available[i] = this.getResources().get(i).getVal();
-		}
-		
-		this.setBankersTable(bankersTable);
-		this.setTotal(total);
-		this.setAvailable(available);
-	}
-	
-	protected void populateTasks() {
-		int[][] processedInput = this.getProcessedInput();
-		ArrayList<Task> tasks = new ArrayList<Task>(this.getNumOfTasks());
-		int mod = this.getMod();
-		int numOfResources = this.getNumOfResources();
-		ArrayList<int[]> initiate = new ArrayList<int[]>(numOfResources);
-		ArrayList<int[]> request = new ArrayList<int[]>(numOfResources);
-		ArrayList<int[]> release = new ArrayList<int[]>(numOfResources);
-		Task t = new Task();
-		for (int i = 0; i < processedInput.length; i++) {
-			int index = (i+1) % mod;
-			if (index == 0) {
-				t = new Task();
-			} else {
-				if (index >= 1 && index <= numOfResources) {
-					initiate.add(processedInput[i]);
-					t.setInitiate(initiate);
-				} else if (index >= numOfResources+1 && index <= numOfResources*2) {
-					request.add(processedInput[i]);
-					t.setRequest(request);
-				} else if (index >= (numOfResources*2)+1 && index <= numOfResources*3) {
-					release.add(processedInput[i]);
-					t.setRelease(release);
-				}
-				if (index == mod-1) {
-					int[] requestRemaining = new int[numOfResources];
-					for (int j = 0; j < requestRemaining.length; j++) {
-						requestRemaining[j] = t.getRequest().get(j)[3];
-					}
-					t.setRequestRemaining(requestRemaining);
-					tasks.add(t);
-				}
-			}
-		}
-		this.setTasks(tasks);
-	}
-	
-	protected void BankersAlgorithm() {
-		int[][][] bankersTable = this.getBankersTable();
-//		int[][] processedInput = this.getProcessedInput();
-		ArrayList<Task> tasks = this.getTasks();
-//		LinkedList<Task> granted = new LinkedList<Task>();
-		ArrayList<Task> blocked = new ArrayList<Task>();
-		ArrayList<Task> done = new ArrayList<Task>();
-//		int[] total = this.getTotal();
-		int[] available = this.getAvailable();
-		int numOfResources = this.getNumOfResources();
-		int numOfTasks = this.getNumOfTasks();
-		int finished = 0;
-		int cycle = numOfResources;
-//		boolean canBreak;
-		while (done.size() < numOfTasks) {
-			ArrayList<Task> newBlocked = new ArrayList<Task>();
-			ArrayList<Task> newSafe = new ArrayList<Task>();
-			ArrayList<Task> newDone = new ArrayList<Task>();
-			
-			for (int i = 0; i < tasks.size(); i++) {
-				int count = 0;
-				for (int j = 0; j < numOfResources; j++) {
-					if (tasks.get(i).getRequestRemaining()[j] != 0)
-						break;
-					else
-						count++;
-				}
-				if (count == numOfResources) {
-					newDone.add(tasks.get(i));
-					for (int k = 0; k < available.length; k++) {
-						available[k] += bankersTable[i][1][k];
-					}
-					// Change bankers table to arraylist<object> so we know
-					// which row corresponds to which task in the other
-					// data structures
-				}
-			}
-
-			if (newDone.size() != 0) {
-				for (int i = 0; i < newDone.size(); i++) {
-					done.add(newDone.get(i));
-					tasks.remove(newDone.get(i));
-				}
-			}
-			
-			if (blocked.size() != 0) {
-				for (int i = 0; i < blocked.size(); i++) {
-					// check if any blocked tasks are safe to grant requests
-					ArrayList<int[]> request = blocked.get(i).getRequest();
-					int[] potentialAvailable = new int[numOfResources];
-					int[] potentialGranted = bankersTable[i][2];
-					int[] potentialAllocated = bankersTable[i][1];
-					int[] requestRemaining = blocked.get(i).getRequestRemaining();
-					for (int j = 0; j < potentialGranted.length; j++) {
-						potentialGranted[j] -= request.get(j)[3];
-						potentialAvailable[j] = available[j] - request.get(j)[3];
-						potentialAllocated[j] += request.get(j)[3];
-						if (potentialGranted[j] <= potentialAvailable[j]) {
-							bankersTable[i][2] = potentialGranted;
-							bankersTable[i][1] = potentialAllocated;
-							available[j] = potentialAvailable[j];
-							requestRemaining[j] -= request.get(j)[3];
-						} else {
-							if (!newSafe.contains(blocked.get(i)))
-								newSafe.add(blocked.get(i));
-						}
-					}
-				}
-			}
-			for (int i = 0; i < tasks.size(); i++) {
-				ArrayList<int[]> request = tasks.get(i).getRequest();
-				int[] potentialAvailable = new int[numOfResources];
-				int[] potentialGranted = bankersTable[i][2];
-				int[] potentialAllocated = bankersTable[i][1];
-				int[] requestRemaining = tasks.get(i).getRequestRemaining();
-				for (int j = 0; j < potentialGranted.length; j++) {
-					potentialGranted[j] -= request.get(j)[3];
-					potentialAvailable[j] = available[j] - request.get(j)[3];
-					potentialAllocated[j] += request.get(j)[3];
-					if (potentialGranted[j] <= potentialAvailable[j]) {
-						bankersTable[i][2] = potentialGranted;
-						bankersTable[i][1] = potentialAllocated;
-						available[j] = potentialAvailable[j];
-						requestRemaining[j] -= request.get(j)[3];
-					} else {
-						if (!newBlocked.contains(tasks.get(i)))
-							newBlocked.add(tasks.get(i));
-					}
-				}
-			}
-			if (newSafe.size() != 0) {
-				for (int i = 0; i < newSafe.size(); i++) {
-					tasks.add(newSafe.get(i));
-					blocked.remove(newSafe.get(i));
-				}
-			}
-			if (newBlocked.size() != 0) {
-				for (int i = 0; i < newBlocked.size(); i++) {
-					blocked.add(newBlocked.get(i));
-					tasks.remove(newBlocked.get(i));
-				}
-			}
-			if (blocked.size() == numOfTasks) {
-				System.err.println("WARNING: DEADLOCK");
-				System.exit(0);
-			}
-			cycle++;
-		}
+	protected void FIFO(ArrayList<Object> tasks, ArrayList<Object> resources) {
+		ArrayList<Object> running = new ArrayList<Object>();
+		ArrayList<Object> blocked = new ArrayList<Object>();
+		ArrayList<Object> done = new ArrayList<Object>();
 	}
 	
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.processInput(args[0]);
-		main.populateBankersTable();
-		main.populateTasks();
-		main.BankersAlgorithm();
-	}
-	
-	public ArrayList<Resource> getResources() {
-		return resources;
-	}
-	
-	public void setResources(ArrayList<Resource> resources) {
-		this.resources = resources;
+		ArrayList<ArrayList<Object>> tasksResources = main.processInput(args[0]);
+		ArrayList<Object> tasks = tasksResources.get(0);
+		ArrayList<Object> resources = tasksResources.get(1);
+		
+		main.FIFO(tasks,resources);
 	}
 	
 	public int[][] getProcessedInput() {
@@ -311,11 +130,11 @@ public class Main {
 		this.available = available;
 	}
 
-	public int[][][] getBankersTable() {
+	public HashMap<Task,int[][]> getBankersTable() {
 		return bankersTable;
 	}
 
-	public void setBankersTable(int[][][] bankersTable) {
+	public void setBankersTable(HashMap<Task,int[][]> bankersTable) {
 		this.bankersTable = bankersTable;
 	}
 
@@ -341,5 +160,13 @@ public class Main {
 
 	public void setTasks(ArrayList<Task> tasks) {
 		this.tasks = tasks;
+	}
+
+	public ArrayList<Task> getTasks2() {
+		return tasks2;
+	}
+
+	public void setTasks2(ArrayList<Task> tasks2) {
+		this.tasks2 = tasks2;
 	}
 }
